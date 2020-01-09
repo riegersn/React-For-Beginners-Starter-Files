332089877de40ad35c31c5dd9101d3cd3fde30e5
Shawn Rieger <riegersn@gmail.com>
2020-01-09 14:22:18 -0500

    course steps 1-13

---
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
+        {/* these comments are rediculous */}
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router.jsx';
+import './css/style.css';
+
+render(<Router />, document.querySelector('#main'));
